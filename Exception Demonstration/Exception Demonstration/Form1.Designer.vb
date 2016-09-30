1b4a851bb8f3a0978eb067d3f743bab091cb88ac
Nick-Nahnybida <nicknahnybida@gmail.com>
2016-09-30 10:05:53 -0500

    Add files via upload

---
+++ Exception Demonstration/Exception Demonstration/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.lblSalary = New System.Windows.Forms.Label()
+        Me.txtSalary = New System.Windows.Forms.TextBox()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnOK
+        '
+        Me.btnOK.Location = New System.Drawing.Point(134, 126)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(75, 23)
+        Me.btnOK.TabIndex = 0
+        Me.btnOK.Text = "OK"
+        Me.btnOK.UseVisualStyleBackColor = True
+        '
+        'lblSalary
+        '
+        Me.lblSalary.AutoSize = True
+        Me.lblSalary.Location = New System.Drawing.Point(36, 89)
+        Me.lblSalary.Name = "lblSalary"
+        Me.lblSalary.Size = New System.Drawing.Size(92, 13)
+        Me.lblSalary.TabIndex = 1
+        Me.lblSalary.Text = "Enter Your Salary:"
+        '
+        'txtSalary
+        '
+        Me.txtSalary.Location = New System.Drawing.Point(134, 86)
+        Me.txtSalary.Name = "txtSalary"
+        Me.txtSalary.Size = New System.Drawing.Size(100, 20)
+        Me.txtSalary.TabIndex = 2
+        '
+        'lblResult
+        '
+        Me.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblResult.Location = New System.Drawing.Point(12, 182)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(260, 23)
+        Me.lblResult.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.txtSalary)
+        Me.Controls.Add(Me.lblSalary)
+        Me.Controls.Add(Me.btnOK)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnOK As System.Windows.Forms.Button
+    Friend WithEvents lblSalary As System.Windows.Forms.Label
+    Friend WithEvents txtSalary As System.Windows.Forms.TextBox
+    Friend WithEvents lblResult As System.Windows.Forms.Label
+
+End Class
---
+++ Exception Demonstration/Exception Demonstration/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+
+    Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+        ' Declare a variable to hold the user's salary.
+        Dim decSalary As Decimal
+
+        Try
+            ' Get the users input and convert it to a Decimal.
+            decSalary = CDec(txtSalary.Text)
+
+            ' Display the user's salary.
+            lblResult.Text = "Your salary is " & decSalary.ToString("c")
+        Catch
+            ' Display an error message.
+            lblResult.Text = "Please try again, and enter a number."
+        End Try
+    End Sub
+End Class
